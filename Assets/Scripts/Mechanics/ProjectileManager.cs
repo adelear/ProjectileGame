56ba3ecf2feb03ae9d701e69059dff6934b2f5a2
aledear <veronica.rleda@gmail.com>
2023-10-13 18:12:21 -0400

    Drag and Drop

--- Assets/Scripts/Mechanics/ProjectileManager.cs
+++ Assets/Scripts/Mechanics/ProjectileManager.cs
@@ -16,23 +16,45 @@ public class ProjectileManager : MonoBehaviour
 
     // Release velocity will be used to calculate x and y component 
     //release velocity = 100 units (just an example) 
+    [SerializeField] GameObject slingshotRegion; 
     private Transform projectileTransform;
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Rigidbody2D rb;
 
     private void Start()
     {
-        projectileTransform = transform;
-        SetProjectileScale();
+        rb = GetComponent<Rigidbody2D>();
+        //SetProjectileScale();
     }
 
     private void SetProjectileScale()
     {
-        float randomScale = Random.Range(1.0f, 5.0f);
-        Vector3 newScale = new Vector3(randomScale, randomScale, 1.0f);  
-        projectileTransform.localScale = newScale;
+        float randomScale = Random.Range(0.5f, 1.0f);
+        Vector3 newScale = new Vector3(randomScale, randomScale, 1.0f);
+        transform.localScale = newScale;
     }
-     
-    private void Update()
+
+    private void OnMouseDown()
     {
-       
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rb.velocity = (mousePos + offset - transform.position) * 10;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+        rb.velocity = Vector3.zero;
+        
+        slingshotRegion.SetActive(false); 
     }
 }
